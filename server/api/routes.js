e3ea0baad734c796a31a4c42491fbdbb5baacc9c
Frank van Wijk <fvanwijk@xebia.com>
2018-02-04 22:47:28 +0100

    Rename routes

--- server/api/routes.js
+++ server/api/routes.js
@@ -1,6 +1,7 @@
 const controllers = require('./controller');
 
 module.exports = function (app) {
-  app.route('/tradfri/devices').get(controllers.devices.getAll);
-  app.route('/tradfri/groups').get(controllers.groups.getAll);
+  app.route('/tradfri/device').get(controllers.devices.getAll);
+  app.route('/tradfri/device/:id').put(controllers.devices.control);
+  app.route('/tradfri/group').get(controllers.groups.getAll);
 };
--- src/components/TradfriService.js
+++ src/components/TradfriService.js
@@ -2,9 +2,9 @@ const getJson = path => fetch(path).then(res => res.json());
 
 export default {
   getDevices() {
-    return getJson('/api/tradfri/devices');
+    return getJson('/api/tradfri/device');
   },
   getGroups() {
-    return getJson('/api/tradfri/groups');
+    return getJson('/api/tradfri/group');
   },
 };
