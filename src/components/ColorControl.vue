4e5f912c6e9c8941311380a66b1e7795580d63d8
Frank van Wijk <fvanwijk@xebia.com>
2018-03-31 01:16:45 +0200

    Add support for controlling color of Philips hue by sending the CIE xy coordinates instead of RGB string

--- src/components/ColorControl.vue
+++ src/components/ColorControl.vue
@@ -8,6 +8,8 @@
 </style>
 <script>
 import TradfriService from './TradfriService';
+import { rgb } from 'd3-color';
+import { rgb_to_cie } from '../lib/cieRgbConverter';
 
 export default {
   data() {
@@ -17,8 +19,13 @@ export default {
   },
   methods: {
     setColor(value) {
+      const { r: red, g: green, b: blue } = rgb(value);
+      const [a, b] = rgb_to_cie(red, green, blue);
+
       TradfriService.controlDevice(this.instanceId, {
-        color: value.slice(1),
+        //color: value.slice(1), Not supported by Philips Hue bulbs
+        colorX: Math.round(a * 65279),
+        colorY: Math.round(b * 65279)
       });
     },
   },
